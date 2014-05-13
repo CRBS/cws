07b978e6ffecd8ba19e30c1b16bf7e1392ef00c3
Christopher Churas <churas@gmail.com>
2014-05-13 16:58:05 -0700

    added more query params

--- cws-war/src/main/java/edu/ucsd/crbs/cws/rest/Constants.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/rest/Constants.java
@@ -17,5 +17,15 @@ public class Constants {
     public static final String NOPARAMS_QUERY_PARAM = "noparams";
     public static final String NOWORKFLOWPARAMS_QUERY_PARAM = "noworkflowparams";
     public static final String NOTSUBMITTED_TO_SCHED_QUERY_PARAM = "notsubmittedtoscheduler";
+    
+    public static final String ESTCPU_QUERY_PARAM = "estcpu";
+    public static final String ESTRUNTIME_QUERY_PARAM = "estruntime";
+    public static final String ESTDISK_QUERY_PARAM = "estdisk";
+    public static final String SUBMITDATE_QUERY_PARAM = "submitdate";
+    public static final String STARTDATE_QUERY_PARAM = "startdate";
+    public static final String FINISHDATE_QUERY_PARAM = "finishdate";
+    public static final String SUBMITTED_TO_SCHED_QUERY_PARAM = "submittedtosched";
+    public static final String DOWNLOADURL_QUERY_PARAM = "downloadurl";
+    
 
 }
