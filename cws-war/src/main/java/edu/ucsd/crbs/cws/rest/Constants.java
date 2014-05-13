198f6ba383cf3258daf7f3aaf3be3fb408c34514
Christopher Churas <churas@gmail.com>
2014-05-13 15:40:04 -0700

    initial revision

---
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/rest/Constants.java
@@ -0,0 +1,21 @@
+package edu.ucsd.crbs.cws.rest;
+
+/**
+ *
+ * @author Christopher Churas <churas@ncmir.ucsd.edu>
+ */
+public class Constants {
+    
+    public static final String TASKS_PATH = "tasks";
+    
+    public static final String WORKFLOWS_PATH = "workflows";
+    
+    public static final String REST_PATH = "rest";
+    
+    public static final String STATUS_QUERY_PARAM = "status";
+    public static final String OWNER_QUERY_PARAM = "owner";
+    public static final String NOPARAMS_QUERY_PARAM = "noparams";
+    public static final String NOWORKFLOWPARAMS_QUERY_PARAM = "noworkflowparams";
+    public static final String NOTSUBMITTED_TO_SCHED_QUERY_PARAM = "notsubmittedtoscheduler";
+
+}
