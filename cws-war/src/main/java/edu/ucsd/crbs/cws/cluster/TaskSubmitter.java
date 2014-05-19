886fb7a74177f931333f65e1934038df51c20a6f
Christopher Churas <churas@gmail.com>
2014-05-19 16:58:05 -0700

    constructor now takes 3 arguments, path to task exec directory, workflow directory, and path to kepler script

--- cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskSubmitter.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskSubmitter.java
@@ -13,10 +13,15 @@ public class TaskSubmitter {
     TaskCmdScriptSubmitter _cmdScriptSubmitter;
     
     
-    
-    public TaskSubmitter(){
-        _directoryCreator = new TaskDirectoryCreatorImpl();
-        _cmdScriptCreator = new TaskCmdScriptCreatorImpl();
+    /**
+     * Constructor
+     * @param workflowExecDir Directory under where workflow Tasks should be run
+     * @param workflowsDir Directory where workflows are stored
+     * @param keplerScript  Full path to Kepler program
+     */
+    public TaskSubmitter(final String workflowExecDir,final String workflowsDir,final String keplerScript){
+        _directoryCreator = new TaskDirectoryCreatorImpl(workflowExecDir);
+        _cmdScriptCreator = new TaskCmdScriptCreatorImpl(workflowsDir,keplerScript);
         _cmdScriptSubmitter = new TaskCmdScriptSubmitterImpl();
     }
     
