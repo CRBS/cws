4c3c53d419b3f242247dedc78bcd325d968b436d
Christopher Churas <churas@gmail.com>
2014-05-21 14:21:23 -0700

    added javadoc

--- cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptSubmitter.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptSubmitter.java
@@ -3,10 +3,20 @@ package edu.ucsd.crbs.cws.cluster;
 import edu.ucsd.crbs.cws.workflow.Task;
 
 /**
- *
+ * Classes implementing this interface can submit or run a script that runs
+ * a Workflow Task
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 public interface TaskCmdScriptSubmitter {
     
-    public String submit(final String cmdScript,Task t) throws Exception;
+    /**
+     * Run the cmdScript either directly or by submitting to some
+     * batch processing system
+     * @param cmdScript Full path to program/script to run
+     * @param t Task that will be run by cmdScript
+     * @return jobid from batch processing system to track job progress
+     * @throws Exception 
+     */
+    public String submit(final String cmdScript,
+            Task t) throws Exception;
 }
