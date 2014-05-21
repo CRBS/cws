b897a9bb979251f0152881d26e1a9296dce4703e
Christopher Churas <churas@gmail.com>
2014-05-21 14:41:48 -0700

    code now sets CWS_taskname, CWS_user, and CWS_outputdir variables with correct values.  Added better javadoc

--- cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptCreatorImpl.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptCreatorImpl.java
@@ -9,42 +9,104 @@ import java.io.File;
 import java.util.LinkedHashMap;
 
 /**
- *
+ * Creates a {@link TASK_CMD_SH} script that can run the Workflow Task
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
-public class TaskCmdScriptCreatorImpl implements TaskCmdScriptCreator,StringReplacer {
+public class TaskCmdScriptCreatorImpl implements TaskCmdScriptCreator, StringReplacer {
 
-    
     public static final String HYPHEN = "-";
     public static final String SPACE = " ";
-    public static final String TASK_CMD_SH_TEMPLATE = "/taskcmd.sh.template";
+
+    /**
+     * Name of script that will run the Workflow Task
+     */
     public static final String TASK_CMD_SH = "taskCmd.sh";
     
+    /**
+     * Path to {@link TASK_CMD_SH} template script
+     */
+    public static final String TASK_CMD_SH_TEMPLATE = "/taskcmd.sh.template";
+
+    /**
+     * Suffix for Workflow KAR files
+     */
     public static final String WORKFLOW_SUFFIX = ".kar";
 
+    /**
+     * Flags to tell kepler to run in command line mode and to redirect gui display
+     * actors to files
+     */
+    public static final String KEPLER_RUN_CMDLINE_ARGS = " -runwf -redirectgui ";
+
+    /**
+     * Token in {@link TASK_CMD_SH} that will be replaced with path to Kepler script
+     */
     public static final String KEPLER_SH_TOKEN = "@@KEPLER_SH@@";
-    public static final String TASK_ARGS_TOKEN = "@@TASK_ARGS@@";    
+    
+    /**
+     * Token in {@link TASK_CMD_SH} that will be replaced with Kepler command
+     * line arguments needed to run the workflow
+     */
+    public static final String TASK_ARGS_TOKEN = "@@TASK_ARGS@@";
+
+    /**
+     * Special parameter set to output directory of running task
+     */
+    public static final String CWS_OUTPUTDIR = "CWS_outputdir";
+
+    /**
+     * Special parameter set to output directory of running task
+     */
+    public static final String CWS_USER = "CWS_user";
+
+    /**
+     * Special parameter set to output directory of running task
+     */
+    public static final String CWS_TASKNAME = "CWS_taskname";
+
+    /**
+     * Base directory under which the Workflow kar files reside
+     */
     private final String _workflowsDir;
+    
+    /**
+     * Path to Kepler script
+     */
     private final String _keplerScript;
+    
+    /**
+     * Arguments to pass to Kepler script
+     */
     private String _taskArgs;
     
-    private LinkedHashMap<String, String> m_EscapeMap;
+    /**
+     * The working directory for the Workflow Task
+     */
+    private String _workingDir;
+
+    /**
+     * Contains a mapping of ASCII characters to HTML escape codes.  This
+     * is needed to replace certain characters with special characters when
+     * passing arguments to Kepler script
+     */
+    private final LinkedHashMap<String, String> m_EscapeMap;
 
     /**
      * Replaces occurrences of @@TASK_ARGS@@ @@KEPLER_SH@@ with correct values
+     *
      * @param line
-     * @return 
+     * @return
      */
     @Override
     public String replace(String line) {
-        return line.replace(KEPLER_SH_TOKEN, _keplerScript).replace(TASK_ARGS_TOKEN, _taskArgs);
+        return line.replace(KEPLER_SH_TOKEN, _keplerScript).
+                replace(TASK_ARGS_TOKEN, _taskArgs);
     }
 
-    
-    public TaskCmdScriptCreatorImpl(final String workflowsDir,final String keplerScript){
+    public TaskCmdScriptCreatorImpl(final String workflowsDir, final String keplerScript) {
         _workflowsDir = workflowsDir;
         _keplerScript = keplerScript;
-        
+
         m_EscapeMap = new LinkedHashMap<>();
         m_EscapeMap.put("&", "&#38;");
         m_EscapeMap.put(" ", "&#32;");
@@ -62,85 +124,94 @@ public class TaskCmdScriptCreatorImpl implements TaskCmdScriptCreator,StringRepl
         m_EscapeMap.put("=", "&#61;");
         m_EscapeMap.put(">", "&#62;");
     }
-    
+
+    /**
+     * Creates {@link TASK_CMD_SH} script in taskDirectory.  This script can later
+     * be submitted to SGE to run the Workflow Task
+     * @param taskDirectory Should be set to base directory of Workflow Task and 
+     * there should exist a {@link Constants.OUTPUS_DIR_NAME} within this directory
+     * @param t Task to run
+     * @return Full path to {@link TASK_CMD_SH} script that can be used to run the Task
+     * @throws Exception If there is an error, duh
+     */
     @Override
     public String create(final String taskDirectory, Task t) throws Exception {
 
+        _workingDir = taskDirectory + File.separator + Constants.OUTPUTS_DIR_NAME;
 
-        _taskArgs = generateTaskArguments(taskDirectory,t);
-        
+        _taskArgs = generateTaskArguments(taskDirectory, t);
+
         ResourceToExecutableScriptWriterImpl resToFile = new ResourceToExecutableScriptWriterImpl();
-        
-        String taskCmd = taskDirectory+File.separator+Constants.OUTPUTS_DIR_NAME+
-                File.separator+TASK_CMD_SH;
-        
+
+        String taskCmd = _workingDir + File.separator + TASK_CMD_SH;
+
         resToFile.writeResourceToScript(TASK_CMD_SH_TEMPLATE,
-                taskCmd,this);
-        
+                taskCmd, this);
+
         return taskCmd;
     }
 
-    private String generateTaskArguments(final String taskDirectory,Task t) throws Exception{
+    /**
+     * Iterates through Parameters of Task and generates a String of flags and values
+     * @param taskDirectory Base Directory path of Task
+     * @param t Task to generate arguments for
+     * @return String containing flags and values that can be passed to Kepler script
+     * @throws Exception 
+     */
+    private String generateTaskArguments(final String taskDirectory, Task t) throws Exception {
+
         StringBuilder sb = new StringBuilder();
-        
         //set the initial flags to run a workflow and redirect display actors
         // to a directory
-        sb.append(" -runwf -redirectgui ").append(taskDirectory);
-        sb.append(File.separator).append(Constants.OUTPUTS_DIR_NAME);
-        
+        sb.append(KEPLER_RUN_CMDLINE_ARGS).append(_workingDir);
+
+        String value;
         //append all command line arguments
-        for (Parameter param : t.getParameters()){
-            
-            //need to deal with hidden parameters too!!!
-            
+        for (Parameter param : t.getParameters()) {
+
+            //need to deal with special parameters!!!
+            if (param.getName().equals(TaskCmdScriptCreatorImpl.CWS_OUTPUTDIR)) {
+                value = _workingDir;
+            } else if (param.getName().equals(TaskCmdScriptCreatorImpl.CWS_TASKNAME)) {
+                value = t.getName();
+            } else if (param.getName().equals(TaskCmdScriptCreatorImpl.CWS_USER)) {
+                value = t.getOwner();
+            } else {
+                value = param.getValue();
+            }
+
             sb.append(SPACE).append(HYPHEN).append(param.getName()).append(SPACE);
-            sb.append("\"").append(escapeString(param.getValue(),t)).append("\"");
+            sb.append("\"").append(escapeString(value)).append("\"");
         }
-        
+
         //add path to workflow
         sb.append(SPACE).append(_workflowsDir).append(File.separator);
         sb.append(t.getWorkflow().getId()).append(File.separator);
         sb.append(t.getWorkflow().getId()).append(WORKFLOW_SUFFIX);
-        
+
         return sb.toString();
     }
-    
-    
-     /**
-     * Replaces special characters with html codes otherwise the parameters
-     * dont get passed properly
+
+    /**
+     * Replaces special characters with html codes otherwise the parameters dont
+     * get passed properly
      *
-     * @param val String to escape if ${ID} is the value it is replaced with id of WorkflowTask
-     *              if ${OUTPUTDIR} is the value it is replaced with redirectDisplay path
-     *              if ${TASKNAME} is the value it is replaced with JobName set by user or unset if not set by user
+     * @param val String to escape if ${ID} is the value it is replaced with id
+     * of WorkflowTask if ${OUTPUTDIR} is the value it is replaced with
+     * redirectDisplay path if ${TASKNAME} is the value it is replaced with
+     * JobName set by user or unset if not set by user
      * @param t Task needed to pull out replacement values
      */
-    private String escapeString(final String val, Task t) {
+    private String escapeString(final String val) {
         if (val == null) {
             return null;
         }
-        
+
         String tmpVal = val;
 
-        if (val.equals("${TASKNAME}")){
-            if (t.getName() == null || t.getName().matches("^\\s*$")){
-               tmpVal = "unset";
-            }
-            else {
-               tmpVal = t.getName();
-            }            
-        }
-
         for (String k : m_EscapeMap.keySet()) {
-            if (val.equals("${ID}")) {
-                tmpVal = Long.toString(t.getId());
-            } else if (val.equals("${OUTPUTDIR}")) {
-                tmpVal = "unset"; //need to set to outputs directory of task
-            } else {
-                tmpVal = tmpVal.replaceAll(k, m_EscapeMap.get(k));
-            }
+            tmpVal = tmpVal.replaceAll(k, m_EscapeMap.get(k));
         }
         return tmpVal;
     }
-    
 }
