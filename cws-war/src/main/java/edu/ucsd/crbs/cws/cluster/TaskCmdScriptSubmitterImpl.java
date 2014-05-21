8ac73e9a867416ed35f28c5f1caf7da58f71eeca
Christopher Churas <churas@gmail.com>
2014-05-21 14:42:15 -0700

    code now invokes panfishcast to submit the job, but does NOT set the job id properly

--- cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptSubmitterImpl.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/TaskCmdScriptSubmitterImpl.java
@@ -1,19 +1,86 @@
 package edu.ucsd.crbs.cws.cluster;
 
 import edu.ucsd.crbs.cws.workflow.Task;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 /**
+ * Submits Workflow Task Command Script to Panfish for processing
  *
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 public class TaskCmdScriptSubmitterImpl implements TaskCmdScriptSubmitter {
 
+    /**
+     * Submits {@link Task} cmdScript which runs Task represented by t to Panfish
+     * for processing. 
+     * @param cmdScript Full path of script to submit to Panfish
+     * @param t Task that will be run by cmdScript
+     * @return String containing Job Id from Panfish to track job progress
+     * @throws Exception 
+     */
     @Override
-    public String submit(String cmdScript, Task t) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        //cd to cmdScriptDir/outputs
-        //use QSUB to submit cmdScript
-        //parse SGE id and return it
+    public String submit(final String cmdScript,
+            Task t) throws Exception {
+
+        String outputDir = new File(cmdScript).getParentFile().getAbsolutePath();
+
+        ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/panfishcast",
+                "-q", "coleslaw_shadow.q",
+                "-N", getJobName(t),
+                "-o", outputDir + File.separator + "stdout",
+                "-e", outputDir + File.separator + "stderr",
+                cmdScript);
+
+        pb.directory(new File(outputDir));
+        pb.redirectErrorStream(true);
+
+        Process p = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        StringBuilder sb = new StringBuilder();
+
+        String line = reader.readLine();
+
+        while (line != null) {
+            sb.append(line).append("\n");
+            line = reader.readLine();
+        }
+        reader.close();
+
+        if (p.exitValue() != 0){
+            throw new Exception("Non zero exit code received from Panfish: "+sb.toString());
+        }
+        
+        return sb.toString();
+    }
+
+    /**
+     * Generates a job name in a format suitable for Sun/Oracle Grid Engine (SGE).<p/>
+     * 
+     * If {@link Task} owner is null this method will return unset_workflow<br/>
+     * Otherwise method will create a string of format: (owner)_workflow-(task id)<br/>
+     * Ex:  bob_workflow-234232345<p/>
+     * 
+     * In addition, the following characters are replaced with X character:
+     * -- any whitespace characters<br/>
+     * -- non-word characters<br/>
+     * -- digits in the first character of job name     * 
+     * @param t Task generate job name for
+     * @return Job name to pass to Sun/Oracle Grid Engine using -N flag
+     */
+    private String getJobName(final Task t) {
+
+        if (t.getOwner() == null) {
+            return "unset_workflow-" + t.getId();
+        }
+        String tempOwner = t.getOwner();
+        //SGE does not like these characters in job name so lets swap them with X 
+        String spaceTossedOwner = tempOwner.replaceAll("\\s", "X");
+        String digitTossedOwner = spaceTossedOwner.replaceAll("^[0-9]", "X");
+        return digitTossedOwner.replaceAll("\\W|_", "X") + "_workflow-" + t.getId();
     }
 
 }
