ed3b7bee9538ddfc2003fb9ad62145046cc48238
Christopher Churas <churas@gmail.com>
2014-05-23 16:48:31 -0700

    added logRequest to log information from Context

--- cws-war/src/main/java/edu/ucsd/crbs/cws/rest/TaskRestService.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/rest/TaskRestService.java
@@ -3,9 +3,11 @@ package edu.ucsd.crbs.cws.rest;
 import edu.ucsd.crbs.cws.dao.TaskDAO;
 import edu.ucsd.crbs.cws.dao.objectify.TaskObjectifyDAOImpl;
 import edu.ucsd.crbs.cws.workflow.Task;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,54 +16,62 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
- * REST Service to manipulate workflow Task objects.  
- * 
+ * REST Service to manipulate workflow Task objects.
+ *
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
-@Path("/"+Constants.TASKS_PATH)
+@Path("/" + Constants.TASKS_PATH)
 public class TaskRestService {
-    
+
     private static final Logger log
             = Logger.getLogger(TaskRestService.class.getName());
 
     TaskDAO _taskDAO;
-    
+
     public TaskRestService() {
         log.info("In constructor of Tasks() rest service");
         _taskDAO = new TaskObjectifyDAOImpl();
     }
 
     /**
-     * HTTP GET call that gets a list of all tasks.  The list can be filtered with various query
-     * parameters (ie parameters that are in the end of the url ?status=Running&owner=bob).
+     * HTTP GET call that gets a list of all tasks. The list can be filtered
+     * with various query parameters (ie parameters that are in the end of the
+     * url ?status=Running&owner=bob).
      * <p/>
-     * Example GET call for Tasks owned by user <b>foo</b> and in running state<p/>
-     * 
-     * http://.../tasks?status=Running&owner=foo
-     * 
-     * @param status  Only Tasks with given status are returned (?status=)
-     * @param owner  Only Tasks matching this owner are returned (?owner=)
-     * @param noParams  Task parameters are stripped from Task objects returned (?noparams=)
-     * @param noWorkflowParams Workflow Parameters are stripped from Workflow objects within Task objects returned (?noworkflowparams=)
-     * @param notSubmitted Only Tasks that have not been submitted to scheduler are returned (?notsubmittedtoscheduler=)
+     * Example GET call for Tasks owned by user <b>foo</b> and in running
+     * state<p/>
      *
-     * @return List of Task objects in JSON format with media type set to {@link MediaType.APPLICATION_JSON}
+     * http://.../tasks?status=Running&owner=foo
+     *
+     * @param status Only Tasks with given status are returned (?status=)
+     * @param owner Only Tasks matching this owner are returned (?owner=)
+     * @param noParams Task parameters are stripped from Task objects returned
+     * (?noparams=)
+     * @param noWorkflowParams Workflow Parameters are stripped from Workflow
+     * objects within Task objects returned (?noworkflowparams=)
+     * @param notSubmitted Only Tasks that have not been submitted to scheduler
+     * are returned (?notsubmittedtoscheduler=)
+     *
+     * @return List of Task objects in JSON format with media type set to
+     * {@link MediaType.APPLICATION_JSON}
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasks(@QueryParam(Constants.STATUS_QUERY_PARAM) final String status,
-                               @QueryParam(Constants.OWNER_QUERY_PARAM) final String owner,
-                               @QueryParam(Constants.NOPARAMS_QUERY_PARAM) final boolean noParams,
-                               @QueryParam(Constants.NOWORKFLOWPARAMS_QUERY_PARAM) final boolean noWorkflowParams,
-                               @QueryParam(Constants.NOTSUBMITTED_TO_SCHED_QUERY_PARAM) final boolean notSubmitted) {
-        
+            @QueryParam(Constants.OWNER_QUERY_PARAM) final String owner,
+            @QueryParam(Constants.NOPARAMS_QUERY_PARAM) final boolean noParams,
+            @QueryParam(Constants.NOWORKFLOWPARAMS_QUERY_PARAM) final boolean noWorkflowParams,
+            @QueryParam(Constants.NOTSUBMITTED_TO_SCHED_QUERY_PARAM) final boolean notSubmitted,
+            @Context HttpServletRequest request) {
+
         try {
+            logRequest(request);
             return this._taskDAO.getTasks(owner, status, notSubmitted, noParams, noWorkflowParams);
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new WebApplicationException(ex);
         }
     }
@@ -75,11 +85,12 @@ public class TaskRestService {
     @GET
     @Path("/{taskid}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Task getTask(@PathParam("taskid") String taskid) {
+    public Task getTask(@PathParam("taskid") String taskid,
+            @Context HttpServletRequest request) {
         try {
+            logRequest(request);
             return _taskDAO.getTaskById(taskid);
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new WebApplicationException(ex);
         }
     }
@@ -88,6 +99,7 @@ public class TaskRestService {
      * Need updateTask method which consumes @POST along with parameters to
      * update Should take a Task, but use a transaction to load and only modify
      * the fields the caller wants changed
+     *
      * @param taskId
      * @param status
      * @param estCpu
@@ -99,7 +111,7 @@ public class TaskRestService {
      * @param finishDate
      * @param submittedToScheduler
      * @param jobId
-     * @return 
+     * @return
      */
     @POST
     @Path("/{taskid}")
@@ -116,8 +128,10 @@ public class TaskRestService {
             @QueryParam(Constants.FINISHDATE_QUERY_PARAM) final Long finishDate,
             @QueryParam(Constants.SUBMITTED_TO_SCHED_QUERY_PARAM) final Boolean submittedToScheduler,
             @QueryParam(Constants.DOWNLOADURL_QUERY_PARAM) final String downloadURL,
-            @QueryParam(Constants.JOB_ID_QUERY_PARAM)final String jobId) {
+            @QueryParam(Constants.JOB_ID_QUERY_PARAM) final String jobId,
+            @Context HttpServletRequest request) {
 
+        logRequest(request);
         if (taskId != null) {
             log.log(Level.INFO, "task id is: {0}", taskId.toString());
         } else {
@@ -126,11 +140,10 @@ public class TaskRestService {
         }
 
         try {
-            return _taskDAO.update(taskId, status, estCpu, estRunTime, estDisk, 
-                    submitDate, startDate, finishDate, submittedToScheduler, 
-                    downloadURL,jobId);
-        }
-        catch(Exception ex){
+            return _taskDAO.update(taskId, status, estCpu, estRunTime, estDisk,
+                    submitDate, startDate, finishDate, submittedToScheduler,
+                    downloadURL, jobId);
+        } catch (Exception ex) {
             throw new WebApplicationException(ex);
         }
     }
@@ -141,12 +154,33 @@ public class TaskRestService {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Task createTask(Task t) {
+    public Task createTask(Task t, @Context HttpServletRequest request) {
         try {
+            logRequest(request);
             return _taskDAO.insert(t);
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             throw new WebApplicationException(ex);
+        }
+    }
+
+    public static void logRequest(HttpServletRequest request) {
+        if (request != null) {
+            String requestorIp = request.getRemoteAddr();
+            if (requestorIp != null) {
+                log.log(Level.INFO, "requestor ip: {}", requestorIp);
+            }
+
+            Enumeration e = request.getHeaderNames();
+            String header = null;
+            if (e != null) {
+                for (; e.hasMoreElements();) {
+                    header = (String) e.nextElement();
+                    String val = request.getHeader(header);
+                    if (val != null) {
+                        log.log(Level.INFO, "Header: " + header + " == " + val);
+                    }
+                }
+            }
         }
     }
 
