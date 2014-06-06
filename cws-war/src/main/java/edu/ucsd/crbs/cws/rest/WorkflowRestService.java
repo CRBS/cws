10ea00055fba97691e4cf7ecb68c0171068dc9b0
Christopher Churas <churas@gmail.com>
2014-06-06 15:05:19 -0700

    made code utilize EventBuilder and Event objects to assist in logging

--- cws-war/src/main/java/edu/ucsd/crbs/cws/rest/WorkflowRestService.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/rest/WorkflowRestService.java
@@ -7,7 +7,9 @@ import edu.ucsd.crbs.cws.auth.User;
 import edu.ucsd.crbs.cws.auth.Authenticator;
 import edu.ucsd.crbs.cws.auth.AuthenticatorImpl;
 import edu.ucsd.crbs.cws.auth.Permission;
+import edu.ucsd.crbs.cws.dao.EventDAO;
 import edu.ucsd.crbs.cws.dao.WorkflowDAO;
+import edu.ucsd.crbs.cws.dao.objectify.EventObjectifyDAOImpl;
 import edu.ucsd.crbs.cws.workflow.Workflow;
 import java.util.List;
 import java.util.logging.Logger;
@@ -20,7 +22,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import edu.ucsd.crbs.cws.dao.objectify.WorkflowObjectifyDAOImpl;
+import edu.ucsd.crbs.cws.log.Event;
+import edu.ucsd.crbs.cws.log.EventBuilder;
+import edu.ucsd.crbs.cws.log.EventBuilderImpl;
 import edu.ucsd.crbs.cws.servlet.WorkflowFile;
+import java.util.logging.Level;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
@@ -35,42 +41,44 @@ import javax.ws.rs.core.Context;
 @Path("/" + Constants.WORKFLOWS_PATH)
 public class WorkflowRestService {
 
-    private static final Logger log
+    private static final Logger _log
             = Logger.getLogger(WorkflowRestService.class.getName());
 
-    WorkflowDAO _workflowDAO;
+    static Authenticator _authenticator = new AuthenticatorImpl();
 
-     static Authenticator _authenticator = new AuthenticatorImpl();
-    /**
-     * Constructor that by default creates Objectify DAO objects
-     */
-    public WorkflowRestService() {
-        log.info("In constructor of Workflows() rest service");
+    static EventBuilder _eventBuilder = new EventBuilderImpl();
+    
+    static EventDAO _eventDAO = new EventObjectifyDAOImpl();
 
-        _workflowDAO = new WorkflowObjectifyDAOImpl();
-    }
+    static WorkflowDAO _workflowDAO = new WorkflowObjectifyDAOImpl();
 
     /**
      * HTTP GET request on /workflows URI that returns a list of all Workflow
      * objects from WorkflowDAO. If none are found an empty list is returned. If
      * there is an error a 500 response is returned
      *
+     * @param userLogin
+     * @param userToken
+     * @param request
      * @return List of Workflow objects in JSON format
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Workflow> getWorkflows(@QueryParam(Constants.USER_LOGIN_PARAM) final String userLogin,
-                                       @QueryParam(Constants.USER_TOKEN_PARAM) final String userToken,
-                                       @Context HttpServletRequest request) {
+            @QueryParam(Constants.USER_TOKEN_PARAM) final String userToken,
+            @Context HttpServletRequest request) {
         List<Workflow> workflows = null;
         try {
-            TaskRestService.logRequest(request);
-            User user = _authenticator.authenticate(request,userLogin,userToken);
-            if (user.isAuthorizedTo(Permission.LIST_ALL_WORKFLOWS)){
+            User user = _authenticator.authenticate(request, userLogin, userToken);
+            Event event = _eventBuilder.createEvent(request, user);
+            _log.info(event.getStringOfLocationData());
+            
+            if (user.isAuthorizedTo(Permission.LIST_ALL_WORKFLOWS)) {
                 return _workflowDAO.getAllWorkflows(true);
             }
             throw new Exception("Not authorized");
         } catch (Exception ex) {
+            _log.log(Level.SEVERE,"Caught Exception",ex);
             throw new WebApplicationException(ex);
         }
     }
@@ -79,6 +87,9 @@ public class WorkflowRestService {
      * Gets a specific Workflow by id
      *
      * @param wfid
+     * @param userLogin
+     * @param userToken
+     * @param request
      * @return
      */
     @GET
@@ -91,13 +102,16 @@ public class WorkflowRestService {
 
         Workflow wf = null;
         try {
-            TaskRestService.logRequest(request);
-            User user = _authenticator.authenticate(request,userLogin,userToken);
-            if (user.isAuthorizedTo(Permission.LIST_ALL_WORKFLOWS)){
+            User user = _authenticator.authenticate(request, userLogin, userToken);
+            Event event = _eventBuilder.createEvent(request, user);
+            _log.info(event.getStringOfLocationData());
+            
+            if (user.isAuthorizedTo(Permission.LIST_ALL_WORKFLOWS)) {
                 return _workflowDAO.getWorkflowById(wfid);
             }
             throw new Exception("Not authorized");
         } catch (Exception ex) {
+            _log.log(Level.SEVERE,"Caught Exception",ex);
             throw new WebApplicationException(ex);
         }
     }
@@ -106,6 +120,9 @@ public class WorkflowRestService {
      * Creates a new workflow by consuming JSON version of Workflow object
      *
      * @param w
+     * @param userLogin
+     * @param userToken
+     * @param request
      * @return
      */
     @POST
@@ -116,22 +133,37 @@ public class WorkflowRestService {
             @QueryParam(Constants.USER_TOKEN_PARAM) final String userToken,
             @Context HttpServletRequest request) {
         try {
-            TaskRestService.logRequest(request);
-            User user = _authenticator.authenticate(request,userLogin,userToken);
-            if (! user.isAuthorizedTo(Permission.CREATE_WORKFLOW)){
+            User user = _authenticator.authenticate(request, userLogin, userToken);
+             Event event = _eventBuilder.createEvent(request, user);
+            _log.info(event.getStringOfLocationData());
+            
+            if (!user.isAuthorizedTo(Permission.CREATE_WORKFLOW)) {
                 throw new Exception("Not authorized");
             }
-            
+
             Workflow insertedWorkflow = _workflowDAO.insert(w);
 
             //build upload URL and add it to workflow
             BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
             insertedWorkflow.setWorkflowFileUploadURL(blobstoreService.createUploadUrl("/workflowfile",
                     Builder.withGoogleStorageBucketName(WorkflowFile.CLOUD_BUCKET)));
-            return insertedWorkflow;
             
+            saveEvent(_eventBuilder.setAsCreateWorkflowEvent(event, w));
+            
+            return insertedWorkflow;
+
         } catch (Exception ex) {
+            _log.log(Level.SEVERE,"Caught Exception",ex);
             throw new WebApplicationException(ex);
+        }
+    }
+    
+    private void saveEvent(Event event){
+        try {
+           _eventDAO.insert(event);
+        }
+        catch(Exception ex){
+            _log.log(Level.WARNING, "Unable to save Event", ex);
         }
     }
 }
