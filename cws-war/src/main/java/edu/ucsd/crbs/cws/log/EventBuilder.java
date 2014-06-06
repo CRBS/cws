eac842052c15dcf605ab0e89c9fcde1845c5a9ba
Christopher Churas <churas@gmail.com>
2014-06-06 15:02:25 -0700

    Added documentation and changed method names to be more descriptive

---
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/log/EventBuilder.java
@@ -0,0 +1,38 @@
+package edu.ucsd.crbs.cws.log;
+
+import edu.ucsd.crbs.cws.auth.User;
+import edu.ucsd.crbs.cws.workflow.Task;
+import edu.ucsd.crbs.cws.workflow.Workflow;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Creates, Logs, and persists Event objects
+ * 
+ * @author Christopher Churas <churas@ncmir.ucsd.edu>
+ */
+public interface EventBuilder {
+  
+    
+    /**
+     * Logs the request
+     * @param request
+     * @param user 
+     */
+    public Event createEvent(HttpServletRequest request,User user);
+    
+    /**
+     * Converts Event into what is known as a Create Task Event
+     * @param event
+     * @param task
+     * @return Same Event object passed in unless there was a problem in which case null is returned
+     */
+    public Event setAsCreateTaskEvent(Event event,Task task);
+    
+    /**
+     * Converts Event into what is known as a Create Workflow Event
+     * @param event
+     * @param workflow
+     * @return Same Event object passed in unless there was a problem in which case null is returned
+     */
+    public Event setAsCreateWorkflowEvent(Event event,Workflow workflow);
+}
