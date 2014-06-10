af0fdcc0e0b89e189bc8d447febb4fdb83f8cb45
Christopher Churas <churas@gmail.com>
2014-06-10 16:50:21 -0700

    moved getInputStreamOfWorkflowMoml to static method in separate class

--- cws-war/src/main/java/edu/ucsd/crbs/cws/App.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/App.java
@@ -13,6 +13,7 @@ import com.sun.jersey.multipart.impl.MultiPartWriter;
 import edu.ucsd.crbs.cws.cluster.TaskStatusUpdater;
 import edu.ucsd.crbs.cws.cluster.TaskSubmitter;
 import edu.ucsd.crbs.cws.dao.rest.TaskRestDAOImpl;
+import edu.ucsd.crbs.cws.io.KeplerMomlFromKar;
 import edu.ucsd.crbs.cws.rest.Constants;
 import edu.ucsd.crbs.cws.workflow.Parameter;
 import edu.ucsd.crbs.cws.workflow.Task;
@@ -237,7 +238,7 @@ public class App {
 
                 File workflowFile = (File) optionSet.valueOf(UPLOAD_WF_ARG);
                 WorkflowFromXmlFactory xmlFactory = new WorkflowFromXmlFactory();
-                xmlFactory.setWorkflowXml(new BufferedInputStream(getInputStreamOfWorkflowMoml(workflowFile)));
+                xmlFactory.setWorkflowXml(new BufferedInputStream(KeplerMomlFromKar.getInputStreamOfWorkflowMoml(workflowFile)));
 
                 Workflow w = xmlFactory.getWorkflow();
                 if (w != null) {
@@ -350,54 +351,7 @@ public class App {
 
     }
 
-    /**
-     * Returns an input stream to moml file within path passed in. If the path
-     * ends with .xml it is assumed to be a moml file and an inputstream is
-     * opened on it. Otherwise the file is assumed to be a .kar file and is
-     * opened as a zip file and the first entry within that is a file ending
-     * with .xml and has a non zero size is assumed to be a moml file and an
-     * inputstream is opened from that path. <br/>
-     * It is the responsibility of the caller to close the InputStream
-     *
-     * @param workflowFile
-     * @return InputStream object pointing to start of moml file or null if no
-     * moml file is found
-     * @throws Exception
-     */
-    public static InputStream getInputStreamOfWorkflowMoml(final File workflowFile) throws Exception {
-
-        if (workflowFile == null) {
-            throw new NullPointerException("workflow file is null");
-        }
-
-        //if the path ends with .xml assume it is a moml file and return a FileInputStream
-        if (workflowFile.getAbsolutePath().endsWith(XML_SUFFIX)) {
-            return new FileInputStream(workflowFile);
-        }
-
-        //If we are here, we are assuming the file is a kar (compressed zip file)
-        //which is actually a jar file so use JarFile to iterate through all
-        //entries til we find a non zero size entry that is not a directory and
-        //ends with .xml
-        JarFile jf = new JarFile(workflowFile);
-        JarEntry je;
-        for (Enumeration<JarEntry> e = jf.entries(); e.hasMoreElements();) {
-            je = e.nextElement();
-
-            if (je.isDirectory() == true) {
-                continue;
-            }
-            if (je.getSize() == 0 || !je.getName().endsWith(XML_SUFFIX)) {
-                continue;
-            }
-
-            // if we arrived here we found an xml file open a stream to that
-            // file hehehe
-            return jf.getInputStream(je);
-        }
-        //didn't find anything just return null
-        return null;
-    }
+    
 
     public static void renderExampleWorkflowsAndTasksAsJson() throws Exception {
 
