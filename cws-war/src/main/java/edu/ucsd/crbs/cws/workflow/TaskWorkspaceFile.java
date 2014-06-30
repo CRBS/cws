46cb04213cbd783cba8c412b81dfc344a5d36a05
Christopher Churas <churas@gmail.com>
2014-06-30 13:50:09 -0700

    initial revision

---
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/workflow/TaskWorkspaceFile.java
@@ -0,0 +1,84 @@
+/*
+ * COPYRIGHT AND LICENSE
+ * 
+ * Copyright 2014 The Regents of the University of California All Rights Reserved
+ * 
+ * Permission to copy, modify and distribute any part of this CRBS Workflow 
+ * Service for educational, research and non-profit purposes, without fee, and
+ * without a written agreement is hereby granted, provided that the above 
+ * copyright notice, this paragraph and the following three paragraphs appear
+ * in all copies.
+ * 
+ * Those desiring to incorporate this CRBS Workflow Service into commercial 
+ * products or use for commercial purposes should contact the Technology
+ * Transfer Office, University of California, San Diego, 9500 Gilman Drive, 
+ * Mail Code 0910, La Jolla, CA 92093-0910, Ph: (858) 534-5815, 
+ * FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR 
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING 
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS CRBS Workflow Service, EVEN IF 
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * THE CRBS Workflow Service PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE
+ * UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. THE UNIVERSITY OF CALIFORNIA MAKES
+ * NO REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER IMPLIED OR 
+ * EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF 
+ * THE CRBS Workflow Service WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER
+ * RIGHTS. 
+ */
+
+package edu.ucsd.crbs.cws.workflow;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * Links an input {@link WorkspaceFile} with a {@link Task}
+ * @author Christopher Churas <churas@ncmir.ucsd.edu>
+ */
+@Entity
+public class TaskWorkspaceFile {
+
+    @Id private Long _id;
+    @Index private Long _taskId;
+    @Index private Long _workspaceFileId;
+    @Index private boolean _deleted;
+    
+    public Long getId() {
+        return _id;
+    }
+
+    public void setId(Long _id) {
+        this._id = _id;
+    }
+
+    public Long getTaskId() {
+        return _taskId;
+    }
+
+    public void setTaskId(Long _taskId) {
+        this._taskId = _taskId;
+    }
+
+    public Long getWorkspaceFileId() {
+        return _workspaceFileId;
+    }
+
+    public void setWorkspaceFileId(Long _workspaceFileId) {
+        this._workspaceFileId = _workspaceFileId;
+    }
+
+    public boolean isDeleted() {
+        return _deleted;
+    }
+
+    public void setDeleted(boolean _deleted) {
+        this._deleted = _deleted;
+    }
+    
+}
