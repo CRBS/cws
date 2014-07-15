1f818a9da08789f7892c599a94a102d8be67d4cb
Christopher Churas <churas@gmail.com>
2014-07-15 16:56:46 -0700

    added a new line

--- cws-war/src/main/java/edu/ucsd/crbs/cws/rest/Constants.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/rest/Constants.java
@@ -109,6 +109,7 @@ public class Constants {
     public static final String PATH_QUERY_PARAM = "path";
     
     public static final String ADD_UPLOAD_URL_PARAM = "adduploadurl";
+    
     /**
      * outputs Directory where Tasks should write output
      */
