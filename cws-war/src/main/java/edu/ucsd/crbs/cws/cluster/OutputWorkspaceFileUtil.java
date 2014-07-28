380cfca02cd80d76f993c7f25bec57e2be905417
Christopher Churas <churas@gmail.com>
2014-07-28 16:24:38 -0700

    initial revision

---
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/cluster/OutputWorkspaceFileUtil.java
@@ -0,0 +1,64 @@
+/*
+ * COPYRIGHT AND LICENSE
+ * 
+ * Copyright 2014 The Regents of the University of California All Rights Reserved
+ * 
+ * Permission to copy, modify and distribute any part of this CRBS Workflow 
+ * Service for educational, research and non-profit purposes, without fee, and
+ * without a written agreement is hereby granted, provided that the above 
+ * copyright notice, this paragraph and the following three paragraphs appear
+ * in all copies.
+ * 
+ * Those desiring to incorporate this CRBS Workflow Service into commercial 
+ * products or use for commercial purposes should contact the Technology
+ * Transfer Office, University of California, San Diego, 9500 Gilman Drive, 
+ * Mail Code 0910, La Jolla, CA 92093-0910, Ph: (858) 534-5815, 
+ * FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR 
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING 
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS CRBS Workflow Service, EVEN IF 
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * THE CRBS Workflow Service PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE
+ * UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. THE UNIVERSITY OF CALIFORNIA MAKES
+ * NO REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER IMPLIED OR 
+ * EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF 
+ * THE CRBS Workflow Service WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER
+ * RIGHTS. 
+ */
+
+package edu.ucsd.crbs.cws.cluster;
+
+import edu.ucsd.crbs.cws.workflow.Job;
+import edu.ucsd.crbs.cws.workflow.WorkspaceFile;
+
+/**
+ * Defines methods to create and update {@link WorkspaceFile}s that are output of
+ * {@link Job}s
+ * 
+ * @author Christopher Churas <churas@ncmir.ucsd.edu>
+ */
+public interface OutputWorkspaceFileUtil {
+   
+    /**
+     * Creates and registers a the {@link Job} <b>j</b> Output as a new {@link WorkspaceFile}
+     * @param j
+     * @param outputDirectory
+     * @return
+     * @throws Exception 
+     */
+    public WorkspaceFile createAndRegisterJobOutputAsWorkspaceFile(Job j,final String outputDirectory) throws Exception;
+
+    /**
+     * Updates the path of the output {@link WorkspaceFile} linked to the {@link Job} <b>j</b>
+     * @param j
+     * @param outputDirectory
+     * @return
+     * @throws Exception 
+     */
+    public WorkspaceFile updateJobOutputWorkspaceFilePath(Job j,final String outputDirectory) throws Exception;
+}
