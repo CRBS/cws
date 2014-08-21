9138133b1f0bc7785eacea2d2ae556d9d7c93a2b
Christopher Churas <churas@gmail.com>
2014-08-20 18:33:36 -0700

    added implementation for getWorkflowById

--- cws-war/src/main/java/edu/ucsd/crbs/cws/dao/rest/WorkflowRestDAOImpl.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/dao/rest/WorkflowRestDAOImpl.java
@@ -80,7 +80,17 @@ public class WorkflowRestDAOImpl implements WorkflowDAO {
     
     @Override
     public Workflow getWorkflowById(String workflowId, User user) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ClientConfig cc = new DefaultClientConfig();
+        cc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        Client client = Client.create(cc);
+        client.setFollowRedirects(true);
+        WebResource resource = client.resource(_restURL).path(Constants.REST_PATH).path(Constants.WORKFLOWS_PATH).path(workflowId);
+        MultivaluedMap queryParams = _multivaluedMapFactory.getMultivaluedMap(_user);
+        String json = resource.queryParams(queryParams).accept(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new ObjectifyJacksonModule());
+        return mapper.readValue(json, new TypeReference<Workflow>() {
+        });
     }
 
     @Override
