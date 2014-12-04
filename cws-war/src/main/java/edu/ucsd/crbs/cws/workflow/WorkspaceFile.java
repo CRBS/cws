435d5ce1237b0bca116db26eb30bd88d087b2bb2
Christopher Churas <churas@gmail.com>
2014-12-04 14:00:09 -0800

    improved javadoc and added updateWithChanges method which performs an intelligent update to a WorkspaceFile object

--- cws-war/src/main/java/edu/ucsd/crbs/cws/workflow/WorkspaceFile.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/workflow/WorkspaceFile.java
@@ -33,6 +33,7 @@
 
 package edu.ucsd.crbs.cws.workflow;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.googlecode.objectify.annotation.AlsoLoad;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
@@ -41,7 +42,16 @@ import com.googlecode.objectify.annotation.Index;
 import java.util.Date;
 
 /**
- * Represents a file
+ * Represents a file or directory in the CRBS Workflow Service.<p/>  
+ * These are usually generated when {@link Job} objects are created.<br/>
+ * The method {@link #getPath() } connects this object with the real filesystem 
+ * and is only set once physical the file or directory exists.  
+ * The {@link #getDir() } method denotes whether this object points to a file 
+ * or directory.  <br/>
+ * In addition, these objects can also optionally be stored in GAE cloud storage
+ * in which case {@link #getBlobKey() } will be set with a value.<p/>
+ * 
+ *
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 @Entity
@@ -63,98 +73,196 @@ public class WorkspaceFile {
     @Index private boolean _failed = false;
     @Ignore private String _uploadURL;
     
+    /**
+     * Gets identifier for this {@link WorkspaceFile}.  This is usually generated
+     * by whatever datastore is used in <b><code>DAO</code></b> objects.
+     * @return identifier for <b><code>this</code></b> object
+     */
     public Long getId() {
         return _id;
     }
 
-    public void setId(Long _id) {
-        this._id = _id;
+    /**
+     * Sets identifier for this {@link WorkspaceFile}
+     * @param id identifier to set
+     */
+    public void setId(Long id) {
+        _id = id;
     }
 
+    /**
+     * @return Workspace name
+     */
     public String getName() {
         return _name;
     }
 
-    public void setName(String _name) {
-        this._name = _name;
+    /**
+     * Sets Workspace name
+     * @param name Name to set
+     */
+    public void setName(String name) {
+        this._name = name;
     }
 
+    /**
+     * Usually set to name of {@link Workflow} that generated this object
+     * @return Type of {@link WorkspaceFile}
+     */
     public String getType() {
         return _type;
     }
 
-    public void setType(String _type) {
-        this._type = _type;
+    /**
+     * Usually set to name of {@link Workflow} that generated this object
+     * @param type Type of {@link WorkspaceFile}
+     */
+    public void setType(String type) {
+        this._type = type;
     }
 
+    /**
+     * Gets the owner of this {@link WorkspaceFile}
+     * @return owner of this {@link WorkspaceFile}
+     */
     public String getOwner() {
         return _owner;
     }
 
-    public void setOwner(String _owner) {
-        this._owner = _owner;
+    /**
+     * Sets owner of this {@link WorkspaceFile}
+     * @param owner Owner to set
+     */
+    public void setOwner(String owner) {
+        _owner = owner;
     }
 
+    /**
+     * @return Description for this {@link WorkspaceFile}
+     */
     public String getDescription() {
         return _description;
     }
 
-    public void setDescription(String _description) {
-        this._description = _description;
+    /**
+     * Sets the description for this {@link WorkspaceFile} replacing any 
+     * previously set value
+     * @param description Description to set
+     */
+    public void setDescription(String description) {
+        _description = description;
     }
 
+    /**
+     * @return Gets date this object was created
+     */
     public Date getCreateDate() {
         return _createDate;
     }
 
-    public void setCreateDate(Date _createDate) {
-        this._createDate = _createDate;
+    /**
+     * Sets date object is created, replacing any existing value
+     * @param createDate Date to set as create date
+     */
+    public void setCreateDate(Date createDate) {
+        _createDate = createDate;
     }
 
+    /**
+     * @return Gets size in bytes that this {@link WorkspaceFile} consumes and can be null if unknown
+     */
     public Long getSize() {
         return _size;
     }
 
-    public void setSize(Long _size) {
-        this._size = _size;
+    /**
+     * Sets size in bytes this {@link WorkspaceFile} consumes
+     * @param size Size in bytes
+     */
+    public void setSize(Long size) {
+        _size = size;
     }
 
+    /**
+     * @return md5 checksum of this {@link WorkspaceFile} if set
+     */
     public String getMd5() {
         return _md5;
     }
 
+    /**
+     * Sets md5 checksum of this {@link WorkspaceFile}
+     * @param _md5 
+     */
     public void setMd5(String _md5) {
         this._md5 = _md5;
     }
 
+    /**
+     * Denotes whether this {@link WorkspaceFile} has been logically deleted
+     * @return <b><code>true</code></b> if this {@link WorkspaceFile} has been logically deleted <b><code>false</code></b> otherwise
+     */
     public boolean getDeleted() {
         return _deleted;
     }
 
+    /**
+     * Sets whether this {@link WorkspaceFile} has been logically deleted
+     * @param deleted <b><code>true</code></b> if this {@link WorkspaceFile} has 
+     * been logically deleted <b><code>false</code></b> otherwise
+     */
     public void setDeleted(boolean deleted) {
-        this._deleted = deleted;
+        _deleted = deleted;
     }
 
+    /**
+     * Denotes whether this {@link WorkspaceFile} represents a directory
+     * @return <b><code>true</code></b> if this {@link WorkspaceFile} represents 
+     * a directory <b><code>false</code></b> otherwise
+     */
     public boolean getDir() {
         return _dir;
     }
 
+    /**
+     * Sets whether this {@link WorkspaceFile} represents a directory replacing
+     * any previously set value
+     * @param isDir <b><code>true</code></b> if this {@link WorkspaceFile} 
+     * represents a directory <b><code>false</code></b> otherwise
+     */
     public void setDir(boolean isDir) {
         _dir = isDir;
     }
 
+    /**
+     * Gets path on physical file system for this {@link WorkspaceFile}
+     * @return full path on physical file system or <b><code>null</code></b>
+     */
     public String getPath() {
         return _path;
     }
 
-    public void setPath(String _path) {
-        this._path = _path;
+    /**
+     * Sets path on physical file system for this {@link WorkspaceFile} replacing
+     * any value previously set
+     * @param path full path on physical file system or <b><code>null</code></b>
+     */
+    public void setPath(String path) {
+        _path = path;
     }
 
+    /**
+     * Gets identifier of {@link Job} that generated this {@link WorkspaceFile}
+     * @return {@link Job} id
+     */
     public Long getSourceJobId() {
         return _sourceJobId;
     }
 
+    /**
+     * Sets identifier of {@link Job} that generated this {@link WorkspaceFile}
+     * @param sourceJobId {@link Job} id
+     */
     public void setSourceJobId(Long sourceJobId) {
         this._sourceJobId = sourceJobId;
     }
@@ -175,11 +283,152 @@ public class WorkspaceFile {
         this._uploadURL = _uploadURL;
     }
     
+    /**
+     * Denotes whether this {@link WorkspaceFile} was generated from a {@link Job}
+     * ,linked via {@link #getSourceJobId() }, that failed to run correctly.  
+     * @return <b><code>true</code></b> if the {@link #getSourceJobId() } failed <b><code>false</code></b> otherwise
+     */
     public boolean isFailed() {
         return _failed;
     }
 
+    /**
+     * Sets whether this {@link WorkspaceFile} was generated from a {@link Job}
+     * ,linked via {@link #getSourceJobId() }, that failed to run correctly.  
+     * @param failed <b><code>true</code></b> if the {@link #getSourceJobId() } failed <b><code>false</code></b> otherwise
+     */
     public void setFailed(boolean failed) {
         _failed = failed;
+    }
+    
+    /**
+     * Updates this {@link WorkspaceFile} with any changes found in <b>wsf</b> passed
+     * in.  This implementation examines the following fields and compares them
+     * if they are <b>NOT <code>null</code></b><br/>
+     * <ul>
+     * {@link #getBlobKey() }<br/>
+     * {@link #getCreateDate() }<br/>
+     * {@link #getDescription() }<br/>
+     * {@link #getMd5() }<br/>
+     * {@link #getName() }<br/>
+     * {@link #getOwner() }<br/>
+     * {@link #getPath() }<br/>
+     * {@link #getSize() }<br/>
+     * {@link #getSourceJobId() }<br/>
+     * {@link #getType() }
+     * </ul>
+     * <p/>
+     * 
+     * If any of the above fields are different then what is in <code>this</code><br/>
+     * {@link WorkspaceFile} then this {@link WorkspaceFile} is updated and 
+     * this method returns <code>true</code><br/>
+     * In addition, if any of these parameters <b>isDeleted, isFailed, or isDir</b> are 
+     * <b>NOT <code>null</code></b><br/> and different then the value in this 
+     * {@link WorkspaceFile}.  Then they are used to update {@link #getDeleted()},
+     * {@link #isFailed() }, and {@link #getDir() } respectively
+     * 
+     * 
+     * @param wsf
+     * @param isDeleted <b>NOT null and <code>true</code></b> 
+     * @param isFailed
+     * @param isDir
+     * @return <b><code>true</code></b> if <b><code>this</code></b> object was updated with values from <b>wsf</b> passed in otherwise <b><code>false</code></b>
+     */
+    @JsonIgnore
+    public boolean updateWithChanges(WorkspaceFile wsf,Boolean isDeleted,
+            Boolean isFailed,Boolean isDir){
+        if (wsf == null){
+            return false;
+        }
+        boolean updated = false;
+        
+        if (isDeleted != null && isDeleted != getDeleted()){
+            setDeleted(isDeleted);
+            updated = true;
+        }   
+        
+        if (isFailed != null && isFailed != isFailed()){
+            setFailed(isFailed);
+            updated = true;
+        }
+        
+        if (isDir != null && isDir != getDir()){
+            setDir(isDir);
+            updated = true;
+        }
+        if (wsf.getBlobKey() != null){
+            if (getBlobKey() == null ||
+                !getBlobKey().equals(wsf.getBlobKey())){
+                setBlobKey(wsf.getBlobKey());
+                updated = true;
+            }
+        }
+        
+        if (wsf.getCreateDate() != null){
+            if (getCreateDate() == null || 
+                !getCreateDate().equals(wsf.getCreateDate())){
+                setCreateDate(wsf.getCreateDate());
+                updated = true;
+            }
+        }
+        if (wsf.getDescription() != null){
+            if (getDescription() == null ||
+                !getDescription().equals(wsf.getDescription())){
+                setDescription(wsf.getDescription());
+                updated = true;
+            }
+        }
+        if (wsf.getMd5() != null){
+            if (getMd5() == null ||
+                !getMd5().equals(wsf.getMd5())){
+                setMd5(wsf.getMd5());
+                updated = true;
+            }
+        }
+        if (wsf.getName() != null){
+            if (getName() == null ||
+                !getName().equals(wsf.getName())){
+                setName(wsf.getName());
+                updated = true;
+            }
+        }
+        
+        if (wsf.getOwner() != null){
+            if (getOwner() == null ||
+                !getOwner().equals(wsf.getOwner())){
+                setOwner(wsf.getOwner());
+                updated = true;
+            }
+        }
+        if (wsf.getPath() != null){
+            if (getPath() == null ||
+                !getPath().equals(wsf.getPath())){
+                setPath(wsf.getPath());
+                updated = true;
+            }
+        }
+        if (wsf.getSize() != null){
+            if (getSize() == null ||
+                wsf.getSize() != getSize()){
+                setSize(wsf.getSize());
+                updated = true;
+            }
+        }
+
+        if (wsf.getSourceJobId() != null){
+            if (getSourceJobId() == null ||
+                getSourceJobId() != wsf.getSourceJobId()){
+                setSourceJobId(wsf.getSourceJobId());
+                updated = true;
+            }
+        }
+        if (wsf.getType() != null){
+            if (getType() == null ||
+                !getType().equals(wsf.getType())){
+                setType(wsf.getType());
+                updated = true;
+            }
+        }
+        return updated;
     }
 }
