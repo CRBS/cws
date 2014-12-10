fcb039835f5a83ca585b339f5222b80a6219381b
Christopher Churas <churas@gmail.com>
2014-12-10 10:31:54 -0800

    added getJobsCount

--- cws-war/src/main/java/edu/ucsd/crbs/cws/dao/JobDAO.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/dao/JobDAO.java
@@ -81,7 +81,23 @@ public interface JobDAO {
             boolean noParams, boolean noWorkflowParams, final Boolean showDeleted) throws Exception;
 
     /**
-     * Inserts a new {@link Job} into data store skipping Workflow Check
+     * Gets count of {@link Job}s using parameters as filters
+     *
+     * @param owner If non null only Jobs with matching owners will be returned
+     * @param status If non null only Jobs with matching status will be returned
+     * @param notSubmittedToScheduler If non null only Jobs matching submitted
+     * to scheduler flag will be returned
+     * @param noParams True means to exclude Parameters in returned Jobs
+     * @param noWorkflowParams True means to exclude WorkflowParameters in
+     * {@link Workflow} objects returned with Jobs
+     * @return
+     * @throws Exception
+     */
+    public int getJobsCount(final String owner, final String status, final Boolean notSubmittedToScheduler,
+            boolean noParams, boolean noWorkflowParams, final Boolean showDeleted) throws Exception;
+    
+    /**
+     * Inserts a new {@link Job} into data store skipping {@link Workflow} Check
      *
      * @param j
      * @param skipWorkflowCheck
