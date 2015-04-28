8e1b975a14e6f15625dd8e0e6cb19f0fb39f5c80
Christopher Churas <churas@gmail.com>
2015-04-28 16:19:39 -0700

    added unimplemented update(WorkspaceFile)

--- cws-war/src/main/java/edu/ucsd/crbs/cws/dao/rest/WorkspaceFileRestDAOImpl.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/dao/rest/WorkspaceFileRestDAOImpl.java
@@ -246,6 +246,12 @@ public class WorkspaceFileRestDAOImpl implements WorkspaceFileDAO {
     }
 
     @Override
+    public WorkspaceFile update(WorkspaceFile wsp) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+    @Override
     public WorkspaceFile resave(long workspaceFileId) throws Exception {
          ClientConfig cc = new DefaultClientConfig();
         cc.getClasses().add(StringProvider.class);
