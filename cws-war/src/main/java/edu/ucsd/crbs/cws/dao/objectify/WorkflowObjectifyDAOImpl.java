c9b6419796a641a5607bb4ba8dfab9b09d511a40
Christopher Churas <churas@gmail.com>
2015-05-11 15:56:40 -0700

    re-indented.  updated insert method to call getLatestWorkflowWithName to update version based on highest version of workflow found with same name.  insert will also now fail if name is null.  cs-160

--- cws-war/src/main/java/edu/ucsd/crbs/cws/dao/objectify/WorkflowObjectifyDAOImpl.java
+++ cws-war/src/main/java/edu/ucsd/crbs/cws/dao/objectify/WorkflowObjectifyDAOImpl.java
@@ -30,7 +30,6 @@
  * THE CRBS Workflow Service WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER
  * RIGHTS. 
  */
-
 package edu.ucsd.crbs.cws.dao.objectify;
 
 import com.google.appengine.api.blobstore.BlobInfo;
@@ -49,6 +48,7 @@ import edu.ucsd.crbs.cws.gae.URLFetcherImpl;
 import edu.ucsd.crbs.cws.workflow.Job;
 import edu.ucsd.crbs.cws.workflow.Workflow;
 import edu.ucsd.crbs.cws.workflow.WorkflowParameter;
+import edu.ucsd.crbs.cws.workflow.report.DeleteReport;
 import edu.ucsd.crbs.cws.workflow.report.DeleteReportImpl;
 import java.util.Date;
 import java.util.List;
@@ -56,40 +56,40 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Provides access methods to load, modify, and save Workflow objects to Google App
- * Engine Data store using the Objectify library
- * 
+ * Provides access methods to load, modify, and save Workflow objects to Google
+ * App Engine Data store using the Objectify library
+ *
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
 
     private static final Logger _log
             = Logger.getLogger(WorkflowObjectifyDAOImpl.class.getName());
-    
-    
+
     WorkflowParameterDataFetcher _dropDownFetcher = new URLFetcherImpl();
 
     private JobDAO _jobDAO = null;
-    
-    public WorkflowObjectifyDAOImpl(JobDAO jobDAO){
+
+    public WorkflowObjectifyDAOImpl(JobDAO jobDAO) {
         _jobDAO = jobDAO;
     }
-    
+
     /**
-     * Via a transaction load {@link Workflow} by <b>workflowId</b> and then 
+     * Via a transaction load {@link Workflow} by <b>workflowId</b> and then
      * resave it
+     *
      * @param workflowId
      * @return Saved {@link Workflow}
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     public Workflow resave(final long workflowId) throws Exception {
-         Workflow res = ofy().transact(new Work<Workflow>() {
+        Workflow res = ofy().transact(new Work<Workflow>() {
             @Override
             public Workflow run() {
                 Workflow workflow;
                 try {
-                    workflow = getWorkflowById(Long.toString(workflowId),null);
+                    workflow = getWorkflowById(Long.toString(workflowId), null);
                 } catch (Exception ex) {
                     return null;
                 }
@@ -104,71 +104,87 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
 
         return res;
     }
-    
-    
-    
-    
+
     /**
-     * Adds a new workflow to the data store.  If the Id of the Workflow is set then the
-     * parent field of this Workflow object is set to that Id and this Workflow is given
-     * a new Id.
-     * 
-     * @param w Workflow to add to data store
-     * @return Workflow object with new id added
+     * Adds a new {@link Workflow} to the data store setting 
+     * {@link Workflow#getVersion()} to +1 higher then the highest version
+     * {@link Workflow} with same {@link Workflow#getName()}
+     * @param w {@link Workflow} to add to data store
+     * @return {@link Workflow} object with new id added
      * @throws Exception If there was an error during persistence
      */
     @Override
     public Workflow insert(Workflow w) throws Exception {
-        if (w == null){
-          throw new Exception("Workflow object passed in is null");
-          }
-     
+        if (w == null) {
+            throw new Exception("Workflow object passed in is null");
+        }
+
         //set date if it is null
-      if (w.getCreateDate() == null){
-          w.setCreateDate(new Date());
-      }
-      
-      //if version is 0 or less set it to 1
-      if (w.getVersion() <= 0){
-          w.setVersion(1);
-      }
-      
-      // seems a user is uploading the same workflow twice. 
-      // lets create a new workflow using the id to load
-      // the old workflow and set it as the parent for this
-      // new workflow
-      if (w.getId() != null && w.getId().longValue() > 0){
-          Workflow parentWf = ofy().load().type(Workflow.class).id(w.getId()).now();
-          if (parentWf == null){
-              throw new Exception("Unable to load parent Workflow with id: "+w.getId().toString());
-          }
-          w.setId(null);
-          w.setParentWorkflow(parentWf);
-          int newVersion = parentWf.getVersion()+1;
-          w.setVersion(newVersion);
-      }
-      
-      Key<Workflow> wfKey = ofy().save().entity(w).now();
-      return w;
-    
+        if (w.getCreateDate() == null) {
+            w.setCreateDate(new Date());
+        }
+
+        Workflow latestWorkflow = getLatestWorkflowWithName(w.getName());
+
+        if (latestWorkflow != null){
+            w.setVersion(latestWorkflow.getVersion() + 1);
+        }
+        else {
+            w.setVersion(1);
+        }
+
+        Key<Workflow> wfKey = ofy().save().entity(w).now();
+        return w;
     }
 
     /**
-     * Obtains all Workflows from data store, optionally removing the WorkflowParameters
-     * if omitWorkflowParams is set to true
-     * @param omitWorkflowParams If set to true then WorkflowParameters is set to null
+     * Finds {@link Workflow} with highest version matching <b>name</b> passed
+     * in.
+     *
+     * @param name {@link Workflow} name to filter by
+     * @return {@link Workflow} with highest {@link Workflow#getVersion()} or
+     * null if none found
+     * @throws Exception
+     */
+    private Workflow getLatestWorkflowWithName(final String name) throws Exception {
+        
+        if (name == null){
+            throw new NullPointerException("Workflow name cannot be null");
+        }
+        
+        Query<Workflow> q = ofy().load().type(Workflow.class);
+        q = q.filter("_name", name);
+        List<Workflow> workflows = q.list();
+        Workflow latestWorkflow = null;
+
+        for (Workflow w : workflows) {
+            if (latestWorkflow == null) {
+                latestWorkflow = w;
+            } else if (latestWorkflow.getVersion() < w.getVersion()) {
+                latestWorkflow = w;
+            }
+        }
+        return latestWorkflow;
+    }
+
+    /**
+     * Obtains all Workflows from data store, optionally removing the
+     * WorkflowParameters if omitWorkflowParams is set to true
+     *
+     * @param omitWorkflowParams If set to true then WorkflowParameters is set
+     * to null
      * @param showDeleted
      * @return List of Workflow objects or empty list or null
      */
     @Override
     public List<Workflow> getAllWorkflows(boolean omitWorkflowParams,
             final Boolean showDeleted) {
-        /* @TODO figure out way to make objectify optionally retreive workflow parameters instead of removing them here */
+        
+        /* @TODO figure out way to make objectify optionally retreive workflow 
+        parameters instead of removing them here */
         Query<Workflow> q = ofy().load().type(Workflow.class);
 
-        if (showDeleted != null) {
-            q = q.filter("_deleted", showDeleted);
-        } else {
+        if (showDeleted != null || showDeleted == false) {
             q = q.filter("_deleted", false);
         }
 
@@ -185,15 +201,18 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
     }
 
     /**
-     * Queries Objectify to get Workflow matching id passed in.  
-     * @param workflowId String containing workflow id.  Id must be greater then 0
+     * Queries Objectify to get Workflow matching id passed in.
+     *
+     * @param workflowId String containing workflow id. Id must be greater then
+     * 0
      * @return Workflow object or null if none is found
-     * @throws Exception If workflowId is null or there is an error parsing the numerical id or if there was an error retrieving from Objectify
+     * @throws Exception If workflowId is null or there is an error parsing the
+     * numerical id or if there was an error retrieving from Objectify
      */
     @Override
-    public Workflow getWorkflowById(final String workflowId,User user) throws Exception {
+    public Workflow getWorkflowById(final String workflowId, User user) throws Exception {
         long wfId;
-        if (workflowId == null){
+        if (workflowId == null) {
             throw new IllegalArgumentException("workflow id cannot be null");
         }
         try {
@@ -201,27 +220,26 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
         } catch (NumberFormatException nfe) {
             throw new NumberFormatException("Unable to parse workflow id from: " + workflowId + " : " + nfe.getMessage());
         }
-        
-        if (wfId <= 0){
-            throw new Exception(Long.toString(wfId)+" is not a valid workflow id");
+
+        if (wfId <= 0) {
+            throw new Exception(Long.toString(wfId) + " is not a valid workflow id");
         }
         Workflow w = ofy().load().type(Workflow.class).id(wfId).now();
-        
-        if (w == null){
+
+        if (w == null) {
             return null;
         }
-        
-        if (w.getParameters() != null){
-            for (WorkflowParameter param : w.getParameters()){
-                _dropDownFetcher.fetchAndUpdate(param,user);
+
+        if (w.getParameters() != null) {
+            for (WorkflowParameter param : w.getParameters()) {
+                _dropDownFetcher.fetchAndUpdate(param, user);
             }
         }
         return w;
     }
 
-    
     @Override
-    public Workflow updateBlobKey(final long workflowId,final String key) throws Exception {
+    public Workflow updateBlobKey(final long workflowId, final String key) throws Exception {
         Workflow resWorkflow;
         resWorkflow = ofy().transact(new Work<Workflow>() {
             @Override
@@ -231,13 +249,13 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
                 if (w == null) {
                     return null;
                 }
-                
-                if (w.getBlobKey() == null && key == null){
-                   return w;
+
+                if (w.getBlobKey() == null && key == null) {
+                    return w;
                 }
-                
-                if (w.getBlobKey() != null && key != null && 
-                        w.getBlobKey().equals(key)){
+
+                if (w.getBlobKey() != null && key != null
+                        && w.getBlobKey().equals(key)) {
                     return w;
                 }
                 w.setBlobKey(key);
@@ -245,14 +263,14 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
                 return w;
             }
         });
-        if (resWorkflow == null){
+        if (resWorkflow == null) {
             throw new Exception("There was a problem updating the workflow");
         }
         return resWorkflow;
     }
-    
+
     @Override
-    public Workflow updateDeleted(final long workflowId,final boolean isDeleted) throws Exception {
+    public Workflow updateDeleted(final long workflowId, final boolean isDeleted) throws Exception {
         Workflow resWorkflow;
         resWorkflow = ofy().transact(new Work<Workflow>() {
             @Override
@@ -262,105 +280,102 @@ public class WorkflowObjectifyDAOImpl implements WorkflowDAO {
                 if (w == null) {
                     return null;
                 }
-                
-                if (w.isDeleted() == isDeleted){
-                   return w;
+
+                if (w.isDeleted() == isDeleted) {
+                    return w;
                 }
-                
-                
+
                 w.setDeleted(isDeleted);
                 Key<Workflow> wKey = ofy().save().entity(w).now();
                 return w;
             }
         });
-        if (resWorkflow == null){
+        if (resWorkflow == null) {
             throw new Exception("There was a problem updating the workflow");
         }
         return resWorkflow;
     }
-    
 
     @Override
     public Workflow getWorkflowForJob(Job job, User user) throws Exception {
-        if (job == null){
+        if (job == null) {
             throw new IllegalArgumentException("Job cannot be null");
         }
-        if (job.getWorkflow() == null){
+        if (job.getWorkflow() == null) {
             throw new IllegalArgumentException("No Workflow found");
         }
-        if (job.getWorkflow().getId() == null){
+        if (job.getWorkflow().getId() == null) {
             throw new IllegalArgumentException("Workflow id not found");
         }
         return getWorkflowById(job.getWorkflow().getId().toString(), user);
     }
-    
+
     /**
-     * Deletes {@link Workflow} identified by <b>workflowId</b> logically or
-     * for real depending on value of <b>permanentlyDelete</b> parameter.<p/>
-     * 
-     * This method first sees if any {@link Job}s have been run on {@link Workflow}
-     * if yes then {@link DeleteReportImpl}'s {@link DeleteWorkflowReport#isSuccessful()}
-     * will be set to <b><code>false</b></code> and {@link DeleteWorkflowReport#getReason()}
-     * will be set to the following:<p/>
-     * 
+     * Deletes {@link Workflow} identified by <b>workflowId</b> logically or for
+     * real depending on value of <b>permanentlyDelete</b> parameter.<p/>
+     *
+     * This method first sees if any {@link Job}s have been run on
+     * {@link Workflow} if yes then {@link DeleteReportImpl}'s
+     * {@link DeleteWorkflowReport#isSuccessful()} will be set to
+     * <b><code>false</b></code> and {@link DeleteWorkflowReport#getReason()}
+     * will be set to the following:
+     * <p/>
+     *
      * <code>Cannot delete (NUMBER) jobs have been run under workflow</code>
-     * 
+     *
      * @param workflowId
      * @param permanentlyDelete
      * @return {@link DeleteReportImpl} denoting success or failure
      * @throws Exception If there was an error querying the datastore
      */
     @Override
-    public DeleteReportImpl delete(long workflowId, Boolean permanentlyDelete) throws Exception {
+    public DeleteReport delete(long workflowId, Boolean permanentlyDelete) throws Exception {
 
         DeleteReportImpl dwr = new DeleteReportImpl();
         dwr.setId(workflowId);
         dwr.setSuccessful(false);
         dwr.setReason("Unknown");
-        
-        _log.log(Level.INFO,"Checking if its possible to delete workflow {0} ",workflowId);
-        
-     //look for any jobs associated with workflow
-     int numAssociatedJobs = _jobDAO.getJobsWithWorkflowIdCount(workflowId);
-     
-     //if found add to DeleteReportImpl and return
-     if (numAssociatedJobs > 0){
-        dwr.setReason("Cannot delete "+numAssociatedJobs+
-                " job(s) have been run under workflow");
-        return dwr;
-     }
 
-      //if permanentlyDelete is not null and true then run real delete
-     if (permanentlyDelete != null && permanentlyDelete == true){
-         //need to load workflow and get its blobkey if any
-         Workflow w = getWorkflowById(Long.toString(workflowId), null);
-         if (w == null){
-             dwr.setSuccessful(false);
-             dwr.setReason("No workflow found");
-             return dwr;
-         }
-         if (w.getBlobKey() != null){
-             _log.log(Level.INFO,"Blob key found {0}  Deleting from blobstore",
-                     w.getBlobKey());
-             BlobKey bk = new BlobKey(w.getBlobKey());
-             BlobInfo bInfo = new BlobInfoFactory().loadBlobInfo(bk);
-             if (bInfo == null){
-                 _log.log(Level.WARNING,"No BlobInfo found");
-             }
-             else {      
-                 _log.log(Level.INFO,"Found file {0}, attempting to delete",bInfo.getFilename());
-                 BlobstoreServiceFactory.getBlobstoreService().delete(bk);
-             }
-         }
-         ofy().delete().type(Workflow.class).id(workflowId).now();
-     }
-     else {
-        //else just set _deleted to true 
-        updateDeleted(workflowId, true);
-     }
-     dwr.setSuccessful(true);
-     dwr.setReason(null);
-     return dwr;
+        _log.log(Level.INFO, "Checking if its possible to delete workflow {0} ", workflowId);
+
+        //look for any jobs associated with workflow
+        int numAssociatedJobs = _jobDAO.getJobsWithWorkflowIdCount(workflowId);
+
+        //if found add to DeleteReportImpl and return
+        if (numAssociatedJobs > 0) {
+            dwr.setReason("Cannot delete " + numAssociatedJobs
+                    + " job(s) have been run under workflow");
+            return dwr;
+        }
+
+        //if permanentlyDelete is not null and true then run real delete
+        if (permanentlyDelete != null && permanentlyDelete == true) {
+            //need to load workflow and get its blobkey if any
+            Workflow w = getWorkflowById(Long.toString(workflowId), null);
+            if (w == null) {
+                dwr.setSuccessful(false);
+                dwr.setReason("No workflow found");
+                return dwr;
+            }
+            if (w.getBlobKey() != null) {
+                _log.log(Level.INFO, "Blob key found {0}  Deleting from blobstore",
+                        w.getBlobKey());
+                BlobKey bk = new BlobKey(w.getBlobKey());
+                BlobInfo bInfo = new BlobInfoFactory().loadBlobInfo(bk);
+                if (bInfo == null) {
+                    _log.log(Level.WARNING, "No BlobInfo found");
+                } else {
+                    _log.log(Level.INFO, "Found file {0}, attempting to delete", bInfo.getFilename());
+                    BlobstoreServiceFactory.getBlobstoreService().delete(bk);
+                }
+            }
+            ofy().delete().type(Workflow.class).id(workflowId).now();
+        } else {
+            //else just set _deleted to true 
+            updateDeleted(workflowId, true);
+        }
+        dwr.setSuccessful(true);
+        dwr.setReason(null);
+        return dwr;
     }
 }
-
