2044a177ca25b7e3e85cee8e0fe86529b18c1fe3
Christopher Churas <churas@gmail.com>
2015-05-12 10:02:09 -0700

    script now takes a parameter that sets the application name

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
-mvn clean install
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "$0 <app engine application name>"
+  echo ""
+  echo "Deploys application to GAE setting application"
+  echo "name to argument passed into this script"
+
+  echo ""
+  exit 1
+fi
+
+declare appEngineAppName=$1
+
+mvn clean install -Dappengine.application.name=${appEngineAppName}
 
 if [ $? != 0 ] ; then
   echo "Build failed"
@@ -7,7 +21,7 @@ fi
 
 cd cws-ear
 
-mvn appengine:update
+mvn appengine:update -Dappengine.application.name=${appEngineAppName}
 
 cd ..
 
